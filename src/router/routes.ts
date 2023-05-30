ef67a1b64226a51428788733717dbbea32c72413
Joan Galmés Riera <joan.galmes@gmail.com>
2023-05-30 11:51:42 +0200

    Preparant docker auto-install

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,8 +9,8 @@ const routes: RouteRecordRaw[] = [
       { path: 'usuaris', component: () => import('pages/UsuariList.vue') },
       { path: 'usuari/:id', component: () => import('pages/UsuariForm.vue') },
       { path: 'scripts', component: () => import('pages/Scripts.vue') },
-      { path: 'login', component: () => import('app_common/Login.vue'), props: { urlRedirect: "/usuaris" } },
-      { path: 'logout', component: () => import('app_common/Logout.vue') },
+      { path: 'login', component: () => import('components/common/Login.vue'), props: { urlRedirect: "/usuaris" } },
+      { path: 'logout', component: () => import('components/common/Logout.vue') },
     ],
   },
 
